784a80359e369f58907075be28466400c1d0b133
MounikaReddy15 <60848892+MounikaReddy15@users.noreply.github.com>
2023-03-01 22:16:30 +0530

    setup react & fetch products

---
+++ src/components/Products.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+
+const Products = () => {
+	const [productsList, setProductsList] = useState([]);
+
+	useEffect(() => {
+		const fetchProducts = async () => {
+			const response = await fetch('https://fakestoreapi.com/products/');
+			const products = await response.json();
+			console.log('data', products);
+			setProductsList(products);
+		}
+
+		fetchProducts()
+	}, [])
+
+	return (
+		<div className="product-list">
+			{productsList.map(product => (
+				<div key={product.id} className="product-card">
+					<img src={product.image} alt={product.title} />
+					<div className="product-details">
+						<h3>{product.title}</h3>
+						<p>{product.price}</p>
+					</div>
+				</div>
+			))}
+		</div>
+	)
+}
+
+export default Products
